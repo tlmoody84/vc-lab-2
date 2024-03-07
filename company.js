921b77d5320f0c208a510828a17a6241d88e5611
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-03-07 22:39:28 +0000

    worked on event listener for location

--- company.js
+++ company.js
@@ -1,13 +1,34 @@
 let employees = [
   {
-    "name": "Zoey Clark", "email": "zoey.clark@iou.com", "position": "developer"}
+    "name": "Zoey Clark", "email": "zoey.clark@iou.com", "position": "developer"},
   {
-    "name": "Niko Brown", "email": "niko.brown@iou.com", "position": "architect"}
-  }
-    {
+    "name": "Niko Brown", "email": "niko.brown@iou.com", "position": "architect"},
+ 
+  {  
       "name": "Kino Black", "email": "kino.black@iou.com", "position": "engineer"}
-    }
-]"
+    
+];
 
 
-      
+let locations = (["Miami", "Los Angeles", "Los Vegas"]);
+
+
+let buttonOne = document.getElementById("btn1");
+
+
+buttonOne.addEventListener("click", () =>{
+  let locations = document.querySelector("ul");
+  for (let i = 0; i < offices.length; i++){
+    let li = document.createElement("li");
+    li.textContent = offices[i];
+    locations.appendChild(li);
+  }
+})
+
+
+
+
+
+
+
+         
