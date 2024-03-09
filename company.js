4f2fa720dddf117e2688e1bd5d5f6033f2fea32b
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-03-09 03:58:31 +0000

    updated optional 2

--- company.js
+++ company.js
@@ -4,11 +4,13 @@ let employees = [
     "email": "zoey.clark@iou.com",
     "position": "developer"
   },
+  
   {
     "name": "Niko Brown",
     "email": "niko.brown@iou.com",
     "position": "architect"
   },
+  
   {
     "name": "Kino Black",
     "email": "kino.black@iou.com",
@@ -17,7 +19,7 @@ let employees = [
 ];
 
 function createEmployeeCards() {
-  let employeeCards = document.getElementById("my-cards"); 
+  let employeeCards = document.getElementById("my-cards");
 
   for (let employee of employees) {
     let body = `Contact ${employee.position} at ${employee.email}`;
@@ -39,18 +41,43 @@ createEmployeeCards();
 let buttonThree = document.getElementById("btn3");
 
 let locations = ["Miami", "Los Angeles", "Los Vegas"];
- 
-buttonThree.addEventListener("click",() => {                  
 
-    for (let i = 0; i < locations.length; i++){
-      let li = document.createElement("li");
-      li.textContent = locations[i];
-      ul.appendChild(li);
+buttonThree.addEventListener("click", () => {
+
+  for (let i = 0; i < locations.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = locations[i];
+    ul.appendChild(li);
 
   }
 })
 
 
 
+let customerReviews = [
+  {
+    "reviewerName": "Lovy Blue", "reviewText": "This is the best travel agency and place I have ever worked with. My vacation was perfect!", "stars": 5
+  },
+  {
+    "reviewerName": "Hiro White", "reviewText": "We used the company for our companies trip and everything was top notch. We stayed at the best hotel. Everyone's room was on the same floor, so convenient.", "stars": 5
+  },
+  {
+    "reviewerName": "Kellie Noel", "reviewText": "We used this agency, but they were so short staffed, the wait was a bit too long. The hotels were ok, they could have been better. I am not too sure if I will work with them again.", "stars": 3
+  }
+]
 
 
+
+function displayReviews(){
+  let reviewCards = document.getElementById("review-cards");
+  for (let i = 0; i < customerReviews.length; i++);
+  let body = `Reviewed by ${customerReviews.reviewerName} and received ${customerReviews.stars}`;
+  let card = `
+    <div class="card">
+      <div class="card-title">${customerReviews.reviewText}</div>
+      <div class="card-body">${body}</div>
+    </div>
+  `
+  reviewCards.innerHTML += card;
+      
+}
